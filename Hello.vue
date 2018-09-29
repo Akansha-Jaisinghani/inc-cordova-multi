eefb8ce67ff496f88d615495239904c251fe1905
akanshajaisinghani <akanshajaisinghani@gmail.com>
2018-09-29 09:27:47 +0530

    something

---
+++ Hello.vue
@@ -0,0 +1,12 @@
+<template>
+<div id="app">
+  {{ message }}
+</div>
+
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+</template>
---
+++ src/components/Layout.vue
@@ -0,0 +1,22 @@
+<template>
+<html>
+<body>
+<p align="right"> username </p>
+<p align="leftt"> other details </p>
+<p align="left"> email </p>
+<p align="left"> phone no </p>
+<p align="left"> delivery address </p>
+
+</body>
+</html>
+</template>
+<style>
+<html>
+<body>
+<div style="width:200px; height:200px;background-color:
+aqua; border-radius: 100px;">
+</div>
+
+</body>
+</html>
+</style>
